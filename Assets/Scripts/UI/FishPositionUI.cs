eeda811b8c55784a61df853d06579a21334d2938
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-03-10 14:11:14 +0100

    Finalizes Sprint 1 Visualisation of Fishing + FishOMeter Minigame
    testable in PlayerControls.cs

--- Assets/Scripts/UI/FishPositionUI.cs
+++ Assets/Scripts/UI/FishPositionUI.cs
@@ -7,23 +7,45 @@ namespace UI
     {
         private IMessageHandler eventsBroker;
         [SerializeField] private RectTransform parentRectTransform;
-    
-        private float fullSize;        
+        [SerializeField] private RectTransform captureAreaParentPanel;
+
+        private float minimum;
+        private float maximum;
         
-        void Start()
+        void OnEnable()
         {
             eventsBroker = FindObjectOfType<EventsBroker>();
-            parentRectTransform = GetComponent<RectTransform>();
-        
-            fullSize = parentRectTransform.rect.width;
-            
+
+            eventsBroker.SubscribeTo<UpdateFishZoneUISizeEvent>(UpdateSize);
             eventsBroker.SubscribeTo<UpdateFishUIPositionEvent>(UpdatePosition);
+            
+        }
+        
+        private void UpdateSize(UpdateFishZoneUISizeEvent eventRef)
+        {
+            var size = captureAreaParentPanel.sizeDelta.x * eventRef.width;
+            
+            var sizeDelta = gameObject.GetComponent<RectTransform>().sizeDelta;
+            sizeDelta.x = size;
+            gameObject.GetComponent<RectTransform>().sizeDelta = sizeDelta;
+            
+            minimum = 0;
+            maximum = captureAreaParentPanel.sizeDelta.x;
         }
 
         private void UpdatePosition(UpdateFishUIPositionEvent eventRef)
         {
-            var currentPosition = parentRectTransform.anchoredPosition;
-            parentRectTransform.anchoredPosition = new Vector3(eventRef.positionX * 100, currentPosition.y);
+            var currentPosition = Mathf.Lerp(minimum,maximum, eventRef.positionX);
+
+            var anchoredPosition = parentRectTransform.anchoredPosition;
+            anchoredPosition.x = currentPosition;
+            parentRectTransform.anchoredPosition = anchoredPosition;
+        }
+
+        private void OnDisable()
+        {
+            eventsBroker.UnsubscribeFrom<UpdateFishZoneUISizeEvent>(UpdateSize);
+            eventsBroker.UnsubscribeFrom<UpdateFishUIPositionEvent>(UpdatePosition);
         }
     }
 }
