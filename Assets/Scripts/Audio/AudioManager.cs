2cbc73e5087dcac4e619b1baa50fb1ba4962b72c
Simon Magnusson <simon.magnusson@student.forsbergsskola.se>
2021-03-11 15:04:25 +0100

    Adds 2 audioclips

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using Player;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+   public AudioMixer audioMixer;
+   public AudioSource[] ambianceSounds;
+   public AudioSource[] musicTracks;
+   public AudioSource[] sfx;
+
+   
+}
