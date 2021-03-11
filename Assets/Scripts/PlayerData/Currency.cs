75b4ed39d78e28856850afe1c04f203becb733b2
Jackoberto <jackoberto01@gmail.com>
2021-03-11 10:26:56 +0100

    Adds more debugging to currency and UIEvent

--- Assets/Scripts/PlayerData/Currency.cs
+++ Assets/Scripts/PlayerData/Currency.cs
@@ -1,21 +1,42 @@
 ﻿using EventManagement;
 using Events;
 using Saving;
+using UnityEngine;
 
 namespace PlayerData
 {
     public class Currency : ICurrency
     {
         private readonly ICurrencySaver saver;
+        private int silver;
+        private int gold;
         private const string CurrencyKey = "Currency";
+        private IMessageHandler messageHandler;
         public Currency(ICurrencySaver saver, IMessageHandler messageHandler)
         {
             this.saver = saver;
-            messageHandler.SubscribeTo<IncreaseSilverEvent>(silverEvent => Silver += silverEvent.Silver);
-            messageHandler.SubscribeTo<IncreaseGoldEvent>(goldEvent => Gold += goldEvent.Gold);
+            this.messageHandler = messageHandler;
+            this.messageHandler.SubscribeTo<IncreaseSilverEvent>(silverEvent => Silver += silverEvent.Silver);
+            this.messageHandler.SubscribeTo<IncreaseGoldEvent>(goldEvent => Gold += goldEvent.Gold);
         }
-        public int Silver { get; private set; }
-        public int Gold { get; private set; }
+
+        public int Silver
+        {
+            get => silver;
+            private set 
+            { 
+                silver = value; 
+                Debug.Log($"Current Silver {silver}");
+                messageHandler.Publish(new UpdateSilverUIEvent(silver));
+            }
+        }
+
+        public int Gold
+        {
+            get => gold;
+            private set => gold = value;
+        }
+
         public void Serialize()
         {
             saver.SaveCurrencies(CurrencyKey, this);
