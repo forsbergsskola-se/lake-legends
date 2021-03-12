753432a6ed3e66e15151e7728c65a4e447ec6913
Jackoberto <jackoberto01@gmail.com>
2021-03-13 00:05:36 +0100

    Updates ItemIndexer on start

--- Assets/Editor/SetItemIndexer.cs
+++ Assets/Editor/SetItemIndexer.cs
@@ -1,15 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Items;
 using UnityEditor;
 using UnityEngine;
 
 namespace Editor
 {
+    [InitializeOnLoad]
     public class SetItemIndexer : AssetPostprocessor
     {
+        static SetItemIndexer()
+        {
+            CheckDependenciesAtStart();
+        }
+
+        private static async void CheckDependenciesAtStart()
+        {
+            await Task.Delay(1);
+            CheckAndFixDependencies();
+        }
         public static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
             string[] movedFromAssetPaths)
+        {
+            CheckAndFixDependencies();
+        }
+
+        private static void CheckAndFixDependencies()
         {
             var itemIndexer = AllItems.ItemIndexer;
             if (itemIndexer == null)
@@ -17,18 +35,22 @@ namespace Editor
                 itemIndexer = ScriptableObject.CreateInstance<ItemIndexer>();
                 AssetDatabase.CreateAsset(itemIndexer, "Assets/Resources/Global Item Index.asset");
             }
-            PopulateItemIndexer(itemIndexer);
+            CheckAndFixItemIndexer(itemIndexer);
         }
         
-        private static void PopulateItemIndexer(ItemIndexer itemIndexer)
+        private static void CheckAndFixItemIndexer(ItemIndexer itemIndexer)
         {
-            ClearDictionary(itemIndexer);
             var items = FindAllItems();
-            foreach (var item in items)
+            var enumerable = items.ToList();
+            if (enumerable.All(item => itemIndexer.indexer.ContainsKey(item.ID)))
+                return;
+
+            ClearDictionary(itemIndexer);
+            foreach (var item in enumerable)
             {
-                if (!itemIndexer.indexer.ContainsKey(item.ID))
-                    itemIndexer.indexer.Add(item.ID, item as ScriptableObject);
+                itemIndexer.indexer.Add(item.ID, item as ScriptableObject);
             }
+
             EditorUtility.SetDirty(itemIndexer);
         }
 
