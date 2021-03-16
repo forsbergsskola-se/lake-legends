bc9cfbb97f085d39aa889918f9f799385e7709ae
Jackoberto <jackoberto01@gmail.com>
2021-03-16 09:43:41 +0100

    Cleansup DataBaseSaver

--- Assets/Scripts/Saving/DataBaseSaver.cs
+++ Assets/Scripts/Saving/DataBaseSaver.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Auth;
-using Firebase;
 using Firebase.Database;
-using Firebase.Extensions;
 
 namespace Saving
 {
     public class DataBaseSaver : ISaver
     {
         private readonly IUser user;
+        private const string DataBaseUrl = "https://lakelegends-ebdcd-default-rtdb.europe-west1.firebasedatabase.app/";
 
         public DataBaseSaver(IUser user)
         {
@@ -17,25 +15,16 @@ namespace Saving
         }
         public async Task<string> Load(string key, string defaultValue)
         {
-            var database =
-                FirebaseDatabase.GetInstance("https://lakelegends-ebdcd-default-rtdb.europe-west1.firebasedatabase.app/");
+            var database = FirebaseDatabase.GetInstance(DataBaseUrl);
             var dataSnapshot = await database.GetReference($"{user.ID}/{key}").GetValueAsync();
             return dataSnapshot.Exists ? dataSnapshot.GetRawJsonValue() : defaultValue;
         }
 
-        private async Task<string> LoadAsync(DatabaseReference reference)
-        {
-            var task = await Task.Run(reference.GetValueAsync);
-            return task.GetRawJsonValue();
-        }
-
         public void Save(string key, string value)
         {
-            var database =
-                FirebaseDatabase.GetInstance(
-                    "https://lakelegends-ebdcd-default-rtdb.europe-west1.firebasedatabase.app/");
-            var refe = database.GetReference($"{user.ID}/{key}");
-            refe.SetRawJsonValueAsync(value);
+            var database = FirebaseDatabase.GetInstance(DataBaseUrl);
+            var dataBaseRef = database.GetReference($"{user.ID}/{key}");
+            dataBaseRef.SetRawJsonValueAsync(value);
         }
     }
 }
