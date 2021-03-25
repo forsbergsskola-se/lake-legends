d2af20ba18768c1b7cd01189f500ad5e88c1e987
Jackoberto <jackoberto01@gmail.com>
2021-03-25 16:57:18 +0100

    Makes the player start with 100 bait

--- Assets/Scripts/Saving/CurrencySaver.cs
+++ Assets/Scripts/Saving/CurrencySaver.cs
@@ -22,7 +22,7 @@ namespace Saving
 
         public async Task<CurrencySave> LoadCurrencies(string key)
         {
-            var save = await saver.Load(key, serializer.SerializeObject(new CurrencySave(0, 0, 0)));
+            var save = await saver.Load(key, serializer.SerializeObject(new CurrencySave(0, 0, 100)));
             return serializer.DeserializeObject<CurrencySave>(save);
         }
     }
