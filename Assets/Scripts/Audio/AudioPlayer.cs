0b438433ccdc6ec6b3bb15b61871118ad08ec270
Jackoberto <jackoberto01@gmail.com>
2021-03-29 15:02:59 +0200

    Adds a way for designers to call sound events

---
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -0,0 +1,23 @@
+﻿using EventManagement;
+using Events;
+using UnityEngine;
+
+namespace Audio
+{
+    public class AudioPlayer : MonoBehaviour
+    {
+        [SerializeField] private string eventName;
+        private IMessageHandler eventBroker;
+
+        private void Awake()
+        {
+            eventBroker = FindObjectOfType<EventsBroker>();
+        }
+
+        public void PlayAudio()
+        {
+            eventBroker ??= FindObjectOfType<EventsBroker>();
+            eventBroker.Publish(new PlaySoundEvent(SoundType.Sfx, eventName));
+        }
+    }
+}
