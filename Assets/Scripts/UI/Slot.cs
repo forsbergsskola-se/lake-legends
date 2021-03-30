89f4e29e106a4c21cf1d53f98d7ff0b8cde99f28
Jackoberto <jackoberto01@gmail.com>
2021-03-30 12:10:24 +0200

    Fixed Serializing bug
    and added methods to assign default images and Reset Images To default images

--- Assets/Scripts/UI/Slot.cs
+++ Assets/Scripts/UI/Slot.cs
@@ -9,13 +9,30 @@ namespace UI
         public Image background;
         public Image frame;
         public Image middleGround;
+        protected Sprite defaultBackGround;
+        protected Sprite defaultFrame;
+        protected Sprite defaultMiddleGround;
         public abstract void Setup(IItem item, bool hasCaught = true);
+
+        public virtual void SetDefaultImages()
+        {
+            defaultBackGround = background.sprite;
+            defaultFrame = frame.sprite;
+            defaultMiddleGround = middleGround.sprite;
+        }
 
         public virtual void ApplyImages()
         {
             background.sprite = Item.Rarity.background;
             frame.sprite = Item.Rarity.frame;
             middleGround.sprite = Item.Sprite;
+        }
+
+        public virtual void ResetImages()
+        {
+            background.sprite = defaultBackGround;
+            frame.sprite = defaultFrame;
+            middleGround.sprite = defaultMiddleGround;
         }
         public IItem Item { get; protected set; }
     }
