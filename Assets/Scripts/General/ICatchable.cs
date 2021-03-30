c97cd3f087be59a740f97edbec834f99266328c4
Jackoberto <jackoberto01@gmail.com>
2021-03-30 13:50:26 +0200

    you'll now see the background after a successful catch

--- Assets/Scripts/General/ICatchable.cs
+++ Assets/Scripts/General/ICatchable.cs
@@ -1,9 +1,11 @@
+using Items;
 using UnityEngine;
 
 public interface ICatchable
 {
     public string Name { get; }
     public Sprite Sprite { get; }
+    public Rarity Rarity { get; }
     public FloatRange RandomStopTimeRange { get; }
     public FloatRange RandomMoveTimeRange { get; }
     public float CatchableSpeed { get; }
